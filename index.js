1ac6f6bb16d8809296cc6302a033bfdec06e0181
wilson9108 <wilson.pulivelugula@gmail.com>
2025-11-05 23:08:37 +0530

    in index.js writtend the function to change the placeholder names after few seconds

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 let heading = document.querySelector(".heading")
+let nameInput = document.querySelector('[placeholder="fullname"]')
+let emailInput = document.querySelector('[placeholder="email"]')
 
 function updateHeaderName(name){
-    return heading.innerText = name
+     heading.innerText = name
+}
+
+
+function changeInputNames(placeholderName,placeholderEmail){
+    let name = nameInput.setAttribute('placeholder',placeholderName)
+    let email = emailInput.setAttribute('placeholder',placeholderEmail)
 }
 
 setTimeout(()=>{
     updateHeaderName("Git And GitHub")
+    changeInputNames("Enter Github User Name","Enter Github Email")
 },1500)
+
+setTimeout(()=>{
+    updateHeaderName("GIT AND GITHUB")
+    changeInputNames("Enter Name","Enter  Email")
+
+},1000*6)
