c335603f52e11f910179689249b1cfe40b816bf6
wilson9108 <wilson.pulivelugula@gmail.com>
2025-11-05 23:34:02 +0530

    added function for the form heading

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 let heading = document.querySelector(".heading")
 let nameInput = document.querySelector('[placeholder="fullname"]')
 let emailInput = document.querySelector('[placeholder="email"]')
-
+let formHeading = document.querySelector('.form-heading')
+let formContainer = document.querySelector('.form-container')
+let form = document.querySelector('form')
 function updateHeaderName(name){
      heading.innerText = name
+}
+
+function insertFormHeading(){
+    formHeading.style.textAlign="center"
+    formHeading.style.backgroundColor="yellow"
+    formContainer.insertBefore(formHeading,form)
 }
 
 
@@ -15,6 +23,7 @@ function changeInputNames(placeholderName,placeholderEmail){
 setTimeout(()=>{
     updateHeaderName("Git And GitHub")
     changeInputNames("Enter Github User Name","Enter Github Email")
+    insertFormHeading()
 },1500)
 
 setTimeout(()=>{
